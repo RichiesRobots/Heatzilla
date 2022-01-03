34d10400f66c6fa30101f74da1fd1fb436e442c0
RichiesRobots <96834855+RichiesRobots@users.noreply.github.com>
2022-01-03 16:27:53 +0100

    Create espconfig.php

---
+++ espconfig.php
@@ -0,0 +1,29 @@
+?php
+
+
+error_reporting(-1); // reports all errors
+ini_set("display_errors", "1"); // shows all errors
+ini_set("log_errors", 1);
+ini_set("error_log", "/tmp/php-error.log");
+
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'DBUserName');
+define('DB_PASSWORD', 'DBPAssword');
+define('DB_NAME', 'DBName');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
+function val_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+?>
